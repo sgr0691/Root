9b9b6eef7cf14c8b025ef22d88803951190565e9
Sergio Rovira <sgr0691@users.noreply.github.com>
2026-05-29 01:42:50 -0400

    Fill SHA256 checksums for v0.1.0 release binaries

--- homebrew-tap/Formula/root.rb
+++ homebrew-tap/Formula/root.rb
@@ -7,7 +7,7 @@ class Root < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sgr0691/Root/releases/download/v#{version}/root-aarch64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "ff33bb7a3300c4bb4e72ddd21adbb4039ba15dbb5e8c7e828e719d747ec4395b"
 
       def install
         bin.install "root"
@@ -16,7 +16,7 @@ class Root < Formula
 
     if Hardware::CPU.intel?
       url "https://github.com/sgr0691/Root/releases/download/v#{version}/root-x86_64-apple-darwin.tar.gz"
-      sha256 ""
+      sha256 "919db4d137954f6a427c303f52304ca9485886e9d440ffa2e21bc34fadbbcdf1"
 
       def install
         bin.install "root"
@@ -27,7 +27,7 @@ class Root < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sgr0691/Root/releases/download/v#{version}/root-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 ""
+      sha256 "06ac37b650bd34014f1db5151ef02f2215c44f1fbc5bb4ad54cf21a37ca38518"
 
       def install
         bin.install "root"
